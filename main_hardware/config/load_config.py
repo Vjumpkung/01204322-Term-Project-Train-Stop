5fe846688539defae1ffb5bda847f644fd9bb051
vjumpkung <vjumpkung@gmail.com>
2024-02-14 10:57:07 +0700

    update config and mqtt

--- main_hardware/config/load_config.py
+++ main_hardware/config/load_config.py
@@ -2,3 +2,5 @@ SSID = "SSID"
 PASSWORD = "WIFI password"
 MQTT_BROKER = "MQTT Broker IP (or domain)"
 PORT = 1883
+USERNAME = "username"
+PASS = "password"  # mqtt password
--- main_hardware/mqtt/mqtt_setup.py
+++ main_hardware/mqtt/mqtt_setup.py
@@ -1,12 +1,14 @@
 import ubinascii
 from umqtt.simple import MQTTClient
 import machine
-from load_config import MQTT_BROKER, PORT
+from load_config import MQTT_BROKER, PORT, USERNAME, PASSWORD
 
 
-def setup(MQTT_BROKER, PORT):
+def setup():
     CLIENT_ID = ubinascii.hexlify(machine.unique_id())
-    mqttClient = MQTTClient(CLIENT_ID, MQTT_BROKER, PORT, keepalive=60)
+    mqttClient = MQTTClient(
+        CLIENT_ID, MQTT_BROKER, PORT, user=USERNAME, password=PASSWORD, keepalive=60
+    )
     mqttClient.connect()
     print("MQTT : Connected to %s:%s" % (MQTT_BROKER, PORT))
     return mqttClient
