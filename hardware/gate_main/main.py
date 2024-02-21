3542186dc9be30af4ff1ccb6c54d2f2b0f66df83
sinlnw <sinlnw4567@gmail.com>
2024-02-21 11:09:27 +0700

    add test for gate main

---
+++ hardware/gate_main/main.py
@@ -0,0 +1,66 @@
+import machine
+import mqtt_setup as mqtt
+from time import sleep
+from servo import Servo
+from load_config import MQTT_BROKER, PORT
+
+TOPIC_GATE = "crossing1/gates/north"
+GATE_OPEN_COMMAND = "1"
+GATE_CLOSE_COMMAND = "0"
+GATE_OPEN_STATUS = 180
+GATE_CLOSE_STATUS = 0
+gate_status = 0
+motor = Servo(pin=22)
+motor.update_settings(
+    servo_pwm_freq=50,
+    min_u10_duty=26,
+    max_u10_duty=123,
+    min_angle=0,
+    max_angle=180,
+    pin=22,
+)
+
+def func(topic, msg):
+    if topic == TOPIC_GATE:
+        if msg == GATE_OPEN_COMMAND:
+            gate_status = GATE_OPEN_STATUS
+        elif msg == GATE_CLOSE_COMMAND:
+            gate_status = GATE_CLOSE_STATUS
+        
+        moter.move(gate_status)
+        
+    print(topic,msg)
+
+
+
+def move_servo_test():
+    while 1:
+        for i in range(91):
+            motor.move(i)
+            sleep(0.01)
+        for i in range(90, 0, -1):
+            motor.move(i)
+            sleep(0.01)
+
+
+def main():
+    client = mqtt.setup()
+    client.set_callback(func)
+    mqttClient.subscribe(TOPIC_GATE)
+    while(1):
+        mqttClient.check_msg()
+        sleep(0.5)
+        
+    
+
+
+    
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except OSError as e:
+        print("Error: " + str(e))
+        machine.reset()
+
