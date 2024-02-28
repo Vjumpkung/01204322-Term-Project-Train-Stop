9da4b4c368ee985df65db3117b6f404878cab085
vjumpkung <vjumpkung@gmail.com>
2024-02-28 09:05:02 +0700

    PASSWORD mqtt should be PASS

--- main_hardware/mqtt/mqtt_setup.py
+++ main_hardware/mqtt/mqtt_setup.py
@@ -1,13 +1,13 @@
 import ubinascii
 from umqtt.simple import MQTTClient
 import machine
-from load_config import MQTT_BROKER, PORT, USERNAME, PASSWORD
+from load_config import MQTT_BROKER, PORT, USERNAME, PASS
 
 
 def setup():
     CLIENT_ID = ubinascii.hexlify(machine.unique_id())
     mqttClient = MQTTClient(
-        CLIENT_ID, MQTT_BROKER, PORT, user=USERNAME, password=PASSWORD, keepalive=60
+        CLIENT_ID, MQTT_BROKER, PORT, user=USERNAME, password=PASS, keepalive=60
     )
     mqttClient.connect()
     print("MQTT : Connected to %s:%s" % (MQTT_BROKER, PORT))
