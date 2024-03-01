a0d72564f06a768b9f0bfd84243f9c98e53df931
Peerasu <fecownp@gmail.com>
2024-03-01 18:26:43 +0700

    ultrasonic only publish when change state (initial publish to 0)

--- hardware/ultrasonic/ultra.py
+++ hardware/ultrasonic/ultra.py
@@ -2,37 +2,49 @@ from ultrasonic import get_ultra_distance
 
 class Ultra:
     
-    def __init__(self, mqtt_client, lookout_topic, threshold=5, count=0, memo=0):
+    def __init__(self, mqtt_client, lookout_topic, threshold=10, count=0, memo=0, prev_dec=0):
         self.mqtt_client = mqtt_client
         self.lookout_topic = lookout_topic
         self.threshold = threshold
         self.count = count
         self.memo = memo
+        self.prev_dec = prev_dec
+        
+        self.mqtt_client.publish(
+            self.lookout_topic.encode(), str(prev_dec).encode(), retain=True
+        )
+        print(f"Lookouts {self.lookout_topic}: published status {prev_dec}")
     
     def read_and_publish(self):
         dis = get_ultra_distance()
         decision = 0
         
-        if (dis > self.threshold and self.memo > self.threshold) or (dis < self.threshold and self.memo < self.threshold):
+        if (dis < self.threshold and self.memo < self.threshold) or (dis > self.threshold and self.memo > self.threshold):
             self.count += 1
         else:
             self.count = 0
             
         self.memo = dis
         
+        print(f"dis = {dis} cm")
+        
         if self.count >= 3:
             if dis < self.threshold and dis > 0:
                 decision = 1
             else:
                 decision = 0
+                
+            if decision != self.prev_dec:
+                self.mqtt_client.publish(
+                    self.lookout_topic.encode(), str(decision).encode(), retain=True
+                )
+                print(f"Lookouts {self.lookout_topic}: published status {decision}")
+                
+            self.prev_dec = decision
             self.count = 0
         
-        self.mqtt_client.publish(
-            self.lookout_topic.encode(), str(decision).encode(), retain=True
-        )
         
-        print(f"dis = {dis} cm")
-        print(f"Lookouts {self.lookout_topic}: published status {decision}")
+            
     
             
             
