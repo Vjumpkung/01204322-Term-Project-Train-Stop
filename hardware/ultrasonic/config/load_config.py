709878602abed0a5d9fa6ecdf1da93725a8ae006
vjumpkung <vjumpkung@gmail.com>
2024-03-04 14:12:23 +0700

    add DEBUG option

--- hardware/ultrasonic/config/load_config.py
+++ hardware/ultrasonic/config/load_config.py
@@ -2,5 +2,5 @@ SSID = ""
 PASSWORD = ""
 MQTT_BROKER = "mqtt.embedded-train-stop.dynv6.net"
 PORT = 1883
-USERNAME = "trainstop"
-PASS = "stoptrain"  # mqtt password
+USERNAME = ""
+PASS = ""  # mqtt password
